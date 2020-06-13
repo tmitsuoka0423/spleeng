2169f92608c6580f0c8d4ae60a480b48d7699eb0
takahiro <tmitsuoka0423@gmail.com>
2020-06-13 22:33:44 +0900

    リファクタ

--- spleeng-serverless/lambda/line-bot-webhook/index.ts
+++ spleeng-serverless/lambda/line-bot-webhook/index.ts
@@ -2,6 +2,11 @@ import { Client } from "@line/bot-sdk";
 import Authenticator from "./modules/Authenticator";
 import EventHandlerFactory from "./modules/EventHandlerFactory";
 
+const client = new Client({
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+});
+
 export async function handler(event: any) {
   console.log(JSON.stringify(event));
 
@@ -13,11 +18,6 @@ export async function handler(event: any) {
 
   const body = JSON.parse(event.body);
   console.log("body", JSON.stringify(body));
-
-  const client = new Client({
-    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_CHANNEL_SECRET,
-  });
 
   const webhookEvent = body.events[0];
   const messages = await EventHandlerFactory.getHandler(webhookEvent).getMessages();
